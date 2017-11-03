2b0a0bd703cdb92b5ff74f3c4b5e6820eac5aa88
damisgarcia <damisgarcia@gmail.com>
2017-11-03 18:06:56 -0300

    Scaffold sintetizador de voz

--- js/core.js
+++ js/core.js
@@ -32,3 +32,24 @@ var Router = {
         return DrawerMenu.cancel();
     }
 };
+
+var tts = {
+  speak: function(text, onend){
+    var speechSynthesis = new SpeechSynthesisUtterance(text);
+
+    this.tts_config_defaults(speechSynthesis, onend)
+
+    this.play(speechSynthesis)
+  },
+  play: function(speech){
+    window.speechSynthesis.speak(speech);
+  },
+  tts_config_defaults: function(speech, onend){
+    speech.lang = "pt-BR"
+    speech.rate = 1
+    speech.onend = onend
+    speech.onstart = function(event) {
+      return event
+    };
+  }
+};
--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,26 @@ ApplicationConfig.$inject = ['$stateProvider','$urlRouterProvider']
 
 var ApplicationRun = function($rootScope, DrawerMenu){
     $rootScope.$global = GLOBAL
+
+    var speaks = [
+      "Olá,",
+      "qual é o seu nome?",
+      "site chibata",
+      "fabinho developer faz delação premiada, cita bishedson"
+    ]
+
+    var speaks_position = 0
+
+    function onspeakend(){
+      speaks_position++;
+
+      var text = speaks[speaks_position];
+
+      return angular.isDefined(text) ? tts.speak(text, onspeakend) : false
+    }
+
+    tts.speak(speaks[speaks_position], onspeakend);
+
     $rootScope.$on('$stateChangeStart', angular.bind(this, Router.onStateChangeStart, $rootScope, DrawerMenu));
 }
 
@@ -16,5 +36,5 @@ angular.module('application', [
   'ui.router',
   'ui.router.state.events'
 ])
-    .config(ApplicationConfig)
-    .run(ApplicationRun)
+  .config(ApplicationConfig)
+  .run(ApplicationRun)
