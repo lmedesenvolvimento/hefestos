6ba2d3c2798e5020fc650820d06d07bb3b56469e
damisgarcia <damisgarcia@gmail.com>
2018-06-07 16:37:10 -0300

    Repair mdMedia in audio download button

--- scripts/components/uab-get-audio.js
+++ scripts/components/uab-get-audio.js
@@ -1,7 +1,9 @@
 var uabGetAudio = {
   template: ' \
-    <a md-button ng-if="$mdMedia(\'gt-xs\')" target="blank" href="{{$ctrl.href}}" download="{{ $ctrl.href }}"> \
-    <a md-button class="md-icon-button" ng-if="!$mdMedia(\'gt-xs\')" target="blank" href="{{$ctrl.href}}" download="{{ $ctrl.href }}"> \
+    <a md-button ng-show="$root.$mdMedia(\'gt-sm\')" target="blank" href="{{$ctrl.href}}" download="{{ $ctrl.href }}"> \
+      Baixar Audio \
+    </a>\
+    <a md-button class="md-icon-button" ng-if="$root.$mdMedia(\'xs\')" target="blank" href="{{$ctrl.href}}" download="{{ $ctrl.href }}"> \
       <md-icon md-font-icon="icon-get_app" class="icon-24"></md-icon> \
       <md-tooltip md-direction="top">Baixar Audio Aula</md-tooltip> \
     </a> \
